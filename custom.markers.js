1be8046e7fa458cf8b0c83833136f5c4f6617b10
jerryeatsbeans <jerryeatsbeans@gmail.com>
2023-12-29 02:54:56 -0500

    en

--- custom.markers.js
+++ custom.markers.js
@@ -38,47 +38,59 @@ Use the imageScale property if the pin image is too large.
 
 UnminedCustomMarkers = {
 
-    isEnabled: false,
+    isEnabled: true,
 
     markers: [
 
         // Example 1: Simple marker
         {
-            x: -200,
-            z: -200,
-            image: "custom.pin.png",
-            imageAnchor: [0.5, 1],
-            imageScale: 0.5,
-        },
-
-        // Example 2: Marker with text
-        {
-            x: 0,
-            z: 0,
-            image: "custom.pin.png",
-            imageAnchor: [0.5, 1],
-            imageScale: 0.5,
-            text: "Marker with text",
-            textColor: "red", 
-            offsetX: 0,
-            offsetY: 20,
-            font: "bold 20px Calibri,sans serif",
-        },
-
-        // Example 3: Text only
-        {
-            x: 200,
-            z: 200,
-            text: "Text only", 
+            x: -3396,
+            z: 2889,
+            text: "Aubros", 
             textColor: "yellow", 
             offsetX: 0,
             offsetY: 0, 
-            font: "bold 50px Calibri,sans serif",
+            font: "Arial,sans serif",
+        },
+        {
+            x: -3912,
+            z: 4488,
+            text: "Tidewen", 
+            textColor: "yellow", 
+            offsetX: 0,
+            offsetY: 0, 
+            font: "Arial,sans serif",
+        },
+        {
+            x: -4387,
+            z: 561,
+            text: "Starwed", 
+            textColor: "yellow", 
+            offsetX: 0,
+            offsetY: 0, 
+            font: "Arial,sans serif",
         },
 
         // add your markers here
 
-
+        {
+            x: -1302,
+            z: 1283,
+            text: "Cytheria", 
+            textColor: "yellow", 
+            offsetX: 0,
+            offsetY: 0, 
+            font: "Arial,sans serif",
+        },
+        {
+            x: -2006,
+            z: 4373,
+            text: "Ireus", 
+            textColor: "yellow", 
+            offsetX: 0,
+            offsetY: 0, 
+            font: "Arial,sans serif",
+        },
 
 
         // do not delete the following two closing brackets
