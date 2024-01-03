52f6a8630a47b87b7d638ec9d173eab3e9dc14f9
Jerry <120273220+Jerryeatsbeans@users.noreply.github.com>
2024-01-02 22:46:36 -0600

    Update unmined.map.properties.js

--- unmined.map.properties.js
+++ unmined.map.properties.js
@@ -3,7 +3,7 @@ var UnminedMapProperties = {
     minZoom: -2,
     maxZoom: 2,
     defaultZoom: 0,
-    imageFormat: "webp",
+    imageFormat: "jpeg",
     minRegionX: -10,
     minRegionZ: 0,
     maxRegionX: -1,
